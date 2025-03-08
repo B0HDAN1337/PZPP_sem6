f4830fdb80868af9dc292c07a6f738f68607097c
ghost <machuliak1337@gmail.com>
2025-03-08 19:07:57 +0100

    added scrap controller

---
+++ Controllers/ScrapperController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scraper.Controllers
+{
+    public class ScrapperController : Controller
+    {
+        public IActionResult Scrap()
+        {
+            return View();
+        }
+    }
+}
